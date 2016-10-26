af1e4b8b6d9243136f75f9cfb8439644ea3bf58d
deependerch <deepender07@gmail.com>
2016-10-26 10:12:43 +0530

    Script for copying assets using grunt-contrib-copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,53 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-wiredep');
 
   grunt.initConfig({
       
-      wiredep: {
+  copy: {
+  main: {
+    files: [
+      // includes files within path
+      {expand: true, cwd: 'bower_components/bootstrap/dist/css/', src: '**' ,dest: 'assets/static/css'},
+     
+      {expand: true, cwd: 'bower_components/bootstrap/dist/js/', src: '**' ,dest: 'assets/static/js'},
 
-  task: {
-
-    // Point to the files that should be updated when
-    // you run `grunt wiredep`
-    src: [
-       'rmt-leed-app/*.html'
+      {expand: true, cwd: 'bower_components/font-awesome/css', src: '**' ,dest: 'assets/static/css'},
+        
+      {expand: true, cwd: 'bower_components/jquery/dist', src: '**' ,dest: 'assets/static/js'},
+        
+      {expand: true, cwd: 'bower_components/font-awesome/fonts', src: '**' ,dest: 'assets/fonts'},    
+    
+      {expand: true, cwd: 'bower_components/bootstrap/fonts', src: '**' ,dest: 'assets/fonts'},
     ],
-
-    options: {
-      // See wiredep's configuration documentation for the options
-      // you may pass:
-devDependencies: true
-      // https://github.com/taptapship/wiredep#configuration
-    }
+  },
+},
+      
+  injector: {
+ options: {
+    destFile : 'rmt-leed-app/index.html',
+    ignorePath: 'rmt-leed-app/'
+    }, 
+       files:[ {
+                expand: true,
+                cwd: 'rmt-leed-app/assets/static/css/',
+                src: ['*.css'],
+                dest: 'rmt-leed-app/assets/static/css',
+                ext: '.css'
+              },
+             {
+                expand: true,
+                cwd: 'rmt-leed-app/assets/static/js/',
+                src: ['*.js'],
+                dest: 'rmt-leed-app/assets/static/js',
+                ext: '.js'
+              }
+              ],
   }
-}
 
   });
+    
+    grunt.loadNpmTasks('grunt-injector');
+    
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    
+    grunt.registerTask('default', ['copy','injector']);
 };
